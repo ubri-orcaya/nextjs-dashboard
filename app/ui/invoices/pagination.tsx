6a3b0d2ab9e5c5c8ec6af2e366c327a0e8125444
Dr. Ulrich Brinkmann <ulrich.brinkmann@orcaya.com>
2026-04-02 14:42:56 +0200

    Chapter 10

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,8 +4,18 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { useSearchParams, usePathname } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pagenumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pagenumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   // NOTE: Uncomment this code in Chapter 10
 
   // const allPages = generatePagination(currentPage, totalPages);
